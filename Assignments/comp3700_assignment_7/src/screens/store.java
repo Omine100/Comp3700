a557f80e1f369c911447e91e5d51d91641443345
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-09-25 00:04:59 -0500

    assignment 7 update

--- Assignments/comp3700_assignment_7/src/screens/store.java
+++ Assignments/comp3700_assignment_7/src/screens/store.java
@@ -1,12 +1,40 @@
 package screens;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class store {
+public class store extends JFrame {
     private JPanel purchasePanel;
+    private JLabel purchaseLabel;
     private JTextField purchaseNameTextField;
     private JTextField purchaseQuantityTextField;
     private JTextField purchasePriceTextField;
-    private JButton purchaseTotalButton;
-    private JLabel puchaseTotalLable;
+    private JButton purchaseCalculateButton;
+    private JLabel purchaseTotalLabel;
+
+    public store(String title) {
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(purchasePanel);
+        this.pack();
+
+        purchaseCalculateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int quantity;
+                double price;
+
+                quantity = (int)(Integer.parseInt(purchaseQuantityTextField.getText()));
+                price = Double.parseDouble(purchasePriceTextField.getText());
+                purchaseTotalLabel.setText("Total: " + quantity * price);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new store("Mr. Smith's Store");
+        frame.setVisible(true);
+    }
 }
