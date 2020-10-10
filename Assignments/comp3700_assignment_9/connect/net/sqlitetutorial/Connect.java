18dff3bb356fd2e7a2bc6741e9ff44d055cd028b
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2020-10-10 14:35:06 -0500

    Assignment 9 Update

--- Assignments/comp3700_assignment_9/connect/net/sqlitetutorial/Connect.java
+++ Assignments/comp3700_assignment_9/connect/net/sqlitetutorial/Connect.java
@@ -1,23 +1,27 @@
-package connect.net.sqlitetutorial;
+package net.sqlitetutorial;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * I would just like to say that the website tutorial
- * for this assignment was very misleading at first and
- * I was really confused for a couple of hours! Fun times...
- * Fun times.
+ *
+ * @author sqlitetutorial.net
  */
-
- public class Connect {
+public class Connect {
+     /**
+     * Connect to a sample database
+     */
     public static void connect() {
         Connection conn = null;
         try {
-            String url = "jdbc:sqlite:C:/Users/matth/Documents/Programming/Github/Comp3700/Assignments/comp3700_assignment_9/db/store.db";
+            // db parameters
+            String url = "jdbc:sqlite:/Users/matthewbrowning/Documents/Programming/Github/Comp3700/Assignments/comp3700_assignment_9/dbstore.db";
+            // create a connection to the database
             conn = DriverManager.getConnection(url);
+            
             System.out.println("Connection to SQLite has been established.");
+            
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -30,8 +34,10 @@ import java.sql.SQLException;
             }
         }
     }
-
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         connect();
     }
- }
+}
