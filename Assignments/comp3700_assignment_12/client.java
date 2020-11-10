040140993ecc2e57d3e329f561fb4bb9fc83a90e
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2020-11-09 20:47:55 -0600

    Project 12 Update

--- Assignments/comp3700_assignment_12/client.java
+++ Assignments/comp3700_assignment_12/client.java
@@ -1,8 +1,32 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class client {
+    private static final String SERVER_IP = "localhost";
+    private static final int SERVER_PORT = 4999;
+
     public static void main(String[] args) throws IOException {
-        Socket s = new Socket("localhost", 4999);
-    }
+        //Setup
+        Socket s = new Socket(SERVER_IP, SERVER_PORT);
+        System.out.println("[CLIENT] Connected to server.");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("[CLIENT] Enter a product ID: ");
+        int productID = sc.nextInt();
+
+        //Sending input to server
+        PrintWriter pr = new PrintWriter(s.getOutputStream(), true);
+        pr.println(productID);
+
+        //Receiving input from server
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("[CLIENT] Product information: " + str);
+
+        System.out.println("[CLIENT] Closing...");
+        s.close();
+   }
 }
--- Assignments/comp3700_assignment_12/server.java
+++ Assignments/comp3700_assignment_12/server.java
@@ -1,11 +1,70 @@
 import java.net.*;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket ss = new ServerSocket(4999);
-        Socket s = ss.accept();
+    private static final int PORT = 4999;
 
-        System.out.println("client accepted");
+    public static void main(String[] args) throws IOException {
+        //Setup
+        ServerSocket ss = new ServerSocket(PORT);
+        System.out.println("[SERVER] Waiting for client connection...");
+        Socket s = ss.accept();
+        System.out.println("[SERVER] Connected to client.");
+
+        //Receiving input from client
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String productID = bf.readLine();
+        System.out.println("[SERVER] Product ID: " + productID);
+
+        //Connecting to database
+        System.out.println("[SERVER] Connecting to database...");
+        String productInformation = getProduct(productID, s);
+
+        //Sending input to server
+        PrintWriter pr = new PrintWriter(s.getOutputStream(), true);
+        pr.println(productInformation);
+
+        System.out.println("[SERVER] Closing...");
+        s.close();
+        ss.close();
+    }
+
+    public static Connection connect() {
+        //SQLite connection string
+        String url = "jdbc:sqlite:/Users/matthewbrowning/Documents/Programming/Github/Comp3700/Assignments/comp3700_assignment_12/store.db";
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url);
+            System.out.println("[SERVER] Database connected.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static String getProduct(String productID, Socket s) {
+        String Products = "SELECT PRODUCTID, NAME, PRICE, QUANTITY FROM PRODUCTS";
+        String name = "", price = "", quantity = "";
+
+        try (Connection connection = connect()) {
+            Statement ProductsStatements = connection.createStatement();
+            ResultSet ProductsSet = ProductsStatements.executeQuery(Products);
+
+            name = ProductsSet.getString("NAME");
+            price = ProductsSet.getString("PRICE");
+            quantity = ProductsSet.getString("QUANTITY");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        String result = "Name: " + name + " Price: " + price + " Quantity: " + quantity;
+        return result;
     }
 }
